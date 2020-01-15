18f1423d1ded598d4080e05b464a3b48a6b6793b
Terajoel (Jose De Jesus) <jdejesus@teravisiongames.com>
2020-01-15 17:43:22 -0500

    Improved ignore
    adding starting files for project

---
+++ TopDownAbility/Source/TopDownAbility/TopDownAbility.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "TopDownAbility.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TopDownAbility, "TopDownAbility" );
+ 
---
+++ TopDownAbility/Source/TopDownAbility/TopDownAbilityGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TopDownAbilityGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATopDownAbilityGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATopDownAbilityGameMode();
+};
+
+
+
