ad2a473086a316f5d83c06b1956b947dcc3cecdf
ultratroll <josejoeldejesus@gmail.com>
2020-05-11 16:48:48 -0500

    Adding separators

--- TopDownAbility/Source/TopDownAbility/Private/Character/TD_Character.cpp
+++ TopDownAbility/Source/TopDownAbility/Private/Character/TD_Character.cpp
@@ -4,7 +4,7 @@
 #include "../Plugins/Runtime/GameplayAbilities/Source/GameplayAbilities/Public/AbilitySystemComponent.h"
 #include "../Plugins/Runtime/GameplayAbilities/Source/GameplayAbilities/Public/Abilities/GameplayAbility.h"
 
-// Sets default values
+//------------------------------------------------------------------------------------------
 ATD_Character::ATD_Character()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
@@ -13,32 +13,34 @@ ATD_Character::ATD_Character()
 	AbilityComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilityComponent");
 }
 
-// Called when the game starts or when spawned
+//------------------------------------------------------------------------------------------
 void ATD_Character::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
+//------------------------------------------------------------------------------------------
 void ATD_Character::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-// Called to bind functionality to input
+//------------------------------------------------------------------------------------------
 void ATD_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 }
 
+//------------------------------------------------------------------------------------------
 UAbilitySystemComponent* ATD_Character::GetAbilitySystemComponent() const
 {
 	return AbilityComponent;
 }
 
+//------------------------------------------------------------------------------------------
 void ATD_Character::AquireAbility(TSubclassOf<UGameplayAbility> AbilityToAquire)
 {
 	if (AbilityComponent)
